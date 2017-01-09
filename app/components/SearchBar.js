404d4e9d50951789e964784e6abc92f78ccc0d63
cissiesee <496406128@qq.com>
2017-01-09 17:27:55 +0800

    setstate test

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ImmutableRenderMixin from 'react-immutable-render-mixin';
 
 let SearchBar = React.createClass({
-	contextTypes: {
-		store: React.PropTypes.object
-	},
-    //mixins: [ImmutableRenderMixin],
+    contextTypes: {
+        store: React.PropTypes.object
+    },
+    mixins: [ImmutableRenderMixin],
+    getInitialState: function() {
+        return {test: 1};
+    },
+    clickHandler: function(e) {
+        this.setState({test: 2});
+    },
     render() {
-    	let items = this.context.store.getState().items.toArray();
+    	//let items = this.context.store.getState().items.toArray();
     	//console.log(this.state);
         return (
             <div className="pure-form">
-            	<span>{items.length}</span>
-                <input type="text" onKeyUp={this.props.filterItem} placeholder="请输入查找的item" />
+                <input type="text" onKeyUp={this.props.filterItem} onClick={this.clickHandler} placeholder="请输入查找的item" />
             </div>
         )
     }
